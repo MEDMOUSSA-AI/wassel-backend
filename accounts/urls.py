11e1bd55ff244e7e7d6d0254ba6d67e2bb47aa15
MEDMOUSSA-AI <bdymhmdmwsy@gmail.com>
2026-06-28 05:09:55 +0000

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path("login/",               api.login_view,          name="login"),
-    path("register/client/",     api.register_client,     name="register-client"),
-    path("register/restaurant/", api.register_restaurant, name="register-restaurant"),
-    path("register/livreur/",    api.register_livreur,    name="register-livreur"),
-    path("logout/",              api.logout_view,         name="logout"),
-    path("me/",                  api.me_view,             name="me"),
+    path("login/",               api.login_view,           name="login"),
+    path("register/client/",     api.register_client,      name="register-client"),
+    path("register/restaurant/", api.register_restaurant,  name="register-restaurant"),
+    path("register/livreur/",    api.register_livreur,     name="register-livreur"),
+    path("logout/",              api.logout_view,          name="logout"),
+    path("change-password/",     api.change_password_view, name="change-password"),
+    path("me/",                  api.me_view,              name="me"),
 ]
